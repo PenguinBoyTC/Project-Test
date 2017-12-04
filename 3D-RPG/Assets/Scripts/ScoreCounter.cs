827fda171868fbd1dbfac3c61fa5f1c4ae0dd930
Chong Tan <31427334+PenguinBoyTC@users.noreply.github.com>
2017-12-04 11:19:06 -0600

    The newest version

---
+++ 3D-RPG/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+	
+	private Text MP_Text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 3D-RPG/Assets/Scripts/enemy_movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class enemy_movement : MonoBehaviour
+{
+
+    public float enemy_speed = 10f;
+	public float attackDamage;
+    private Transform target;
+    private int wavepiont_index = 0;
+
+    private Text enemy_death_test_text;
+
+    public int testing = 0;
+
+    private GameObject[] players;
+
+    /**
+    * Pre: enemy object created
+    * Post: enemy object initialized by setting target to the first waypoint
+    * return: NA
+    **/
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        target = waypoints.points[waypoints.points.Length - 1];  //starts at point 0, inside start/spawn
+    }
+
+    /**
+    * Pre: enemy object initialized and has target
+    * Post: enemy moves towards waypoint
+    * return: NA
+    **/
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;  //move in direction on next wavepoint via subtracting current from next
+        transform.Translate(dir.normalized * enemy_speed * Time.deltaTime, Space.World);  //fixed speed, deltaTime makes sure speed doesn't depend on framerate
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            foreach (GameObject go in players)
+            {
+				go.GetComponent<PlayerController>().GetHit(attackDamage);
+            }
+
+            if (testing != 0)
+            {
+                enemy_death_test_text = UIController.instance.transform.Find("Tests/Result (4)").GetComponent<Text>();
+                enemy_death_test_text.text = "Passed";
+            }
+            Destroy(gameObject);
+            print("enemy reached base");
+            return;
+        }
+    }
+
+}
